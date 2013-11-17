c02ea89c318165c526002c2dcb2f704b650e9215
Jesse Herrick <jessegrantherrick@gmail.com>
2013-11-16 19:02:40 -0500

    Add a private method for less lines.

--- lib/gravatar-api/get.rb
+++ lib/gravatar-api/get.rb
@@ -1,12 +1,21 @@
 class Gravatar
+	public
 	def self.url(email, options = { :size => 80 })
-		hash = Gravatar.hash!(email)
-		return "http://www.gravatar.com/avatar/#{hash}/#{options[:size]}"
+		get_url(email)
 	end
 
 	def url(email = @@email)
-		hash = Gravatar.hash!(email)
-		return "http://www.gravatar.com/avatar/#{hash}"
+		get_url(email)
 	end
 	alias :get :url
+
+	private
+	def get_url(email)
+		hash = Gravatar.hash!(email)
+		if options[:size] == 80
+			return "http://www.gravatar.com/avatar/#{hash}"
+		else
+			return "http://www.gravatar.com/avatar/#{hash}/#{options[:size]}"
+		end
+	end
 end
