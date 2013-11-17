3d9f3007a21c3616d470e5c042247334889f0d53
Jesse Herrick <jessegrantherrick@gmail.com>
2013-11-17 12:40:54 -0500

    Commenting my code.

--- lib/gravatar-api/get.rb
+++ lib/gravatar-api/get.rb
@@ -1,15 +1,20 @@
 class Gravatar
 	public
+	# A class method to get your Gravatar URL.
+		# A hash of options for size as well.
 	def self.url(email, options = { :size => 80 })
 		get_url(email)
 	end
 
+	# Same as above but as an instance method.
 	def url(email = @@email, options = { :size => 80 })
 		get_url(email)
 	end
+	# Make a new method 'get' the same as the instance method 'url'.
 	alias :get :url
 
 	private
+	# A private class to save lines of code.
 	def get_url(email)
 		hash = Gravatar.hash!(email)
 		if options[:size] == 80
--- lib/gravatar-api/hash.rb
+++ lib/gravatar-api/hash.rb
@@ -1,6 +1,7 @@
 require 'digest/md5'
 
 class Gravatar
+	# MD5 hash an email or string.
 	def self.hash!(email)
 		email.downcase!
 		hash = Digest::MD5.hexdigest(email)
